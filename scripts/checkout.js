2cb787cd3a77409d3e8663175d39ac316bee9da9
kevin <kevin95120@gmail.com>
2025-01-17 20:48:35 +0800

    feat: add updateDeliveryOption function and integrate delivery option selection in checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,76 +1,77 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import formatCurrency from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions } from '../data/deliveryOptions.js';
 
-updateQuantity(null, null);
+
 const today = dayjs();
 
+updateQuantity();
 let cartSummarytHTML = '';
 cart.forEach((cartItem) => {
 
-const productId = cartItem.productId;
-const quantity = cartItem.quantity;
+    const productId = cartItem.productId;
+    const quantity = cartItem.quantity;
 
-let matchingItem;
-products.forEach((product) => {
-    if (productId === product.id) {
-        matchingItem = product;
-    }
-});
+    let matchingItem;
+    products.forEach((product) => {
+        if (productId === product.id) {
+            matchingItem = product;
+        }
+    });
 
-const deliveryOptionId = cartItem.deliveryOptionId;
-let deliveryOption;
-deliveryOptions.forEach((option) => {
-    if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-    }
-});
-const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-const deliveryDateFormatted = deliveryDate.format('dddd, MMMM D');
+    const deliveryOptionId = cartItem.deliveryOptionId;
+    let deliveryOption;
+    deliveryOptions.forEach((option) => {
+        if (option.id === deliveryOptionId) {
+            deliveryOption = option;
+        }
+    });
+    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    const deliveryDateFormatted = deliveryDate.format('dddd, MMMM D');
 
-cartSummarytHTML += 
-    `<div class="cart-item-container js-cart-item-container-${productId}">
-        <div class="delivery-date">
-            Delivery date: ${deliveryDateFormatted}
-        </div>
-
-        <div class="cart-item-details-grid">
-            <img class="product-image"
-            src=${matchingItem.image}>
-
-            <div class="cart-item-details">
-            <div class="product-name">
-                ${matchingItem.name}
-            </div>
-            <div class="product-price">
-                $${(formatCurrency(matchingItem.priceCents))}
-            </div>
-            <div class="product-quantity">
-                <span>
-                Quantity: <span class="quantity-label">${quantity}</span>
-                </span>
-                
-                <span class="update-quantity-link link-primary js-update-link" data-product-id="${productId}">
-                Update
-                </span>
-                <input class="quantity-input">
-                <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${productId}">Save</span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${productId}">
-                Delete
-                </span>
-            </div>
+    cartSummarytHTML += 
+        `<div class="cart-item-container js-cart-item-container-${productId}">
+            <div class="delivery-date">
+                Delivery date: ${deliveryDateFormatted}
             </div>
 
-            <div class="delivery-options">
-            <div class="delivery-options-title">
-                Choose a delivery option:
+            <div class="cart-item-details-grid">
+                <img class="product-image"
+                src=${matchingItem.image}>
+
+                <div class="cart-item-details">
+                <div class="product-name">
+                    ${matchingItem.name}
+                </div>
+                <div class="product-price">
+                    $${(formatCurrency(matchingItem.priceCents))}
+                </div>
+                <div class="product-quantity">
+                    <span>
+                    Quantity: <span class="quantity-label">${quantity}</span>
+                    </span>
+                    
+                    <span class="update-quantity-link link-primary js-update-link" data-product-id="${productId}">
+                    Update
+                    </span>
+                    <input class="quantity-input">
+                    <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${productId}">Save</span>
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${productId}">
+                    Delete
+                    </span>
+                </div>
+                </div>
+
+                <div class="delivery-options">
+                <div class="delivery-options-title">
+                    Choose a delivery option:
+                </div>
+                ${deliveryOptionsHTML(productId, cartItem)}
+                </div>
             </div>
-            ${deliveryOptionsHTML(productId, cartItem)}
-            </div>
-        </div>
-        </div>`;
+            </div>`;
 });
 
 function deliveryOptionsHTML(productId, cartItem) {
@@ -84,7 +85,9 @@ function deliveryOptionsHTML(productId, cartItem) {
         const priceString = priceCents === 0 ? 'FREE Shipping' : `$${formatCurrency(priceCents)} - Shipping`;
         const isChecked = cartItem.deliveryOptionId === deliveryOption.id;
         html += `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" 
+                data-product-id="${productId}"
+                data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio" ${isChecked ? 'checked' : ''}
                 class="delivery-option-input"
                 name="delivery-option-${productId}">
@@ -103,16 +106,11 @@ function deliveryOptionsHTML(productId, cartItem) {
     return html;
 }
 
-
 document.querySelector('.js-order-summary').innerHTML = cartSummarytHTML;
 
 
-function updateQuantity(productId, newQuantity) {
-    cart.forEach((cartItem) => {
-        if (cartItem.productId === productId) {
-            cartItem.quantity = newQuantity;
-        }
-    });
+function updateQuantity() {
+
     let cartQuantity = 0;
     cart.forEach((cartItem) => {
         cartQuantity += cartItem.quantity;
@@ -126,7 +124,7 @@ document.querySelectorAll('.js-delete-link')
         const productId  = link.dataset.productId;
         removeFromCart(productId);
         document.querySelector(`.js-cart-item-container-${productId}`).remove();
-        updateQuantity(null, null);
+        updateQuantity();
     });
 })
 
@@ -168,12 +166,17 @@ document.querySelectorAll('.js-save-quantity-link')
         } else if (newQuantity === 0) {
             removeFromCart(productId);
             document.querySelector(`.js-cart-item-container-${productId}`).remove();
-            updateQuantity(null, null);
         } else {
             quantityLabel.textContent = newQuantity;
+            cart.forEach((cartItem) => {
+                if (cartItem.productId === productId) {
+                    cartItem.quantity = newQuantity;
+                }
+            });
         }
         container.classList.remove('is-editing-quantity');
-        updateQuantity(productId, newQuantity);
+        updateQuantity();
+        
     });
 })
 
@@ -198,36 +201,43 @@ let paymentSummaryHTML = `
 
 <div class="payment-summary-title">
             Order Summary
-          </div>
+        </div>
 
-          <div class="payment-summary-row">
+        <div class="payment-summary-row">
             <div>Items (3):</div>
             <div class="payment-summary-money">$${itemsTotal}</div>
-          </div>
+        </div>
 
-          <div class="payment-summary-row">
+        <div class="payment-summary-row">
             <div>Shipping &amp; handling:</div>
             <div class="payment-summary-money">$4.99</div>
-          </div>
+        </div>
 
-          <div class="payment-summary-row subtotal-row">
+        <div class="payment-summary-row subtotal-row">
             <div>Total before tax:</div>
             <div class="payment-summary-money">$47.74</div>
-          </div>
+        </div>
 
-          <div class="payment-summary-row">
+        <div class="payment-summary-row">
             <div>Estimated tax (10%):</div>
             <div class="payment-summary-money">$4.77</div>
-          </div>
+        </div>
 
-          <div class="payment-summary-row total-row">
+        <div class="payment-summary-row total-row">
             <div>Order total:</div>
             <div class="payment-summary-money">$52.51</div>
-          </div>
+        </div>
 
-          <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary">
             Place your order
-          </button>
+        </button>
 `;
 
 document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+document.querySelectorAll('.js-delivery-option')
+.forEach((element) => {
+    element.addEventListener('click', () => {
+        const { productId, deliveryOptionId } = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+    });
+});
