4496dd8f8ce1726735daf150d9cdfd5c217bbfb4
kevin <kevin95120@gmail.com>
2025-01-18 17:05:31 +0800

    feat: implement cart loading from local storage and add unit tests for addToCart functionality

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,6 @@
-export let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+export let cart = [];
 
+loadFromStorage();
 
 export function getCartQuantity() {
   let cartQuantity = 0;
@@ -9,14 +10,30 @@ export function getCartQuantity() {
   return cartQuantity;
 }
 
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }];
+  }
+
+};
+
 export function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
     let matchingItem;
-    const quantitySelector = document.querySelector(`.js-quantity-slector-product-${productId}`);
-    const quantity = Number(quantitySelector.value);
     cart.forEach((cartItem) => {
       if (productId === cartItem.productId) {
         matchingItem = cartItem;
@@ -24,11 +41,11 @@ export function addToCart(productId) {
     });
   
     if (matchingItem) {
-      matchingItem.quantity += quantity;
+      matchingItem.quantity += 1;
     } else {
       cart.push({
           productId,
-          quantity,
+          quantity: 1,
           deliveryOptionId: '1'
       });
     }
